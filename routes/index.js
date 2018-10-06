e46a46e755eedaaf5a5b7e15c6737eb6f584192a
DoggyCode <mariusrich11@hotmail.com>
2018-10-07 01:54:22 +0200

    added user not found route

--- routes/index.js
+++ routes/index.js
@@ -18,8 +18,9 @@ router.get('/:steam64', function(req, res, next) {
     console.log('Loading data for ' + steam64);
     db.query(`SELECT * FROM users WHERE identifier = 'steam:${steam64}'`, (err, results) => {
         // Handle user not being found
-        if (results.length == 0) return res.render('profile', { name: 'Not found' });
-
+        if (results.length == 0) {
+            return res.status(404).render('message', { msg: 'Profile not found' });
+        }
         // Send user data
         return res.render('profile', results[0]);
     });
